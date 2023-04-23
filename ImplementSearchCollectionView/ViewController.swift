ec329e209310b985bb59e5e6f6081fa63d5cb261
Ivan <igabrielcorreopersonal2@gmail.com>
2023-04-23 10:14:08 -0700

    Initial Commit

---
+++ ImplementSearchCollectionView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ImplementSearchCollectionView
+//
+//  Created by Ivan Gabriel on 23/04/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
